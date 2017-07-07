a43ca18389009318a549ab450d9ee02533fcdb67
Yubo Li <liyubobj@cn.ibm.com>
2017-07-07 17:49:00 +0800

    Remove endless train loop.

--- train.py
+++ train.py
@@ -19,12 +19,7 @@ args = params_setup()
 gnet = GoogLeNet(args=args) #img_size=227,  label_size=label_size, gpu_memory_fraction=0.4, scope_name=scope_name)
 pkl_files = gnet.get_data(dirname=args.model_name, down_sampling=args.down_sampling)
 
-epoch = 0
-while True:
-    for f in pkl_files:
-        X, Y = pickle.load(gzip.open(f, 'rb'))
-        gnet.fit(X, Y, n_epoch=10)
-        print('[pkl_files] done with %s @ %s' % (f, datetime.now()))
-    epoch += 1
-    print("[Finish] all pkl_files been trained %i times." % epoch)
-    
+for f in pkl_files:
+    X, Y = pickle.load(gzip.open(f, 'rb'))
+    gnet.fit(X, Y, n_epoch=5)
+    print('[pkl_files] done with %s @ %s' % (f, datetime.now()))
